641384d92f31923bf59220a65e256f010829b2c8
JackNulty <jdnulty19@gmail.com>
2025-04-01 14:55:37 +0100

    #3.3 DAU calculation and visualisation

--- part3/functions.py
+++ part3/functions.py
@@ -3,3 +3,11 @@ import pandas as pd
 def count_unique_players(file_path, user_column="pid"):
     df = pd.read_csv(file_path)
     return df[user_column].nunique()
+
+def calcualte_dau(file_path, date_column="Time", user_column="pid"):
+    df = pd.read_csv(file_path, parse_dates=[date_column])
+    df["date"] = df[date_column].dt.date
+    dau = df.groupby("date")["pid"].nunique().reset_index()
+    dau.columns = ["date", "DAU"]
+    return dau
+    
