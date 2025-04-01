0d4c4b4c1b0f25b17265817550c268a6f577a6f7
JackNulty <jdnulty19@gmail.com>
2025-04-01 15:04:12 +0100

    #3.4 MAU calculation and visualisation

--- part3/functions.py
+++ part3/functions.py
@@ -10,4 +10,11 @@ def calcualte_dau(file_path, date_column="Time", user_column="pid"):
     dau = df.groupby("date")["pid"].nunique().reset_index()
     dau.columns = ["date", "DAU"]
     return dau
+
+def calculate_mau(file_path, date_column="Time", user_column="pid"):
+    df = pd.read_csv(file_path, parse_dates=[date_column])
+    df["month"] = df[date_column].dt.to_period("M")
+    mau = df.groupby("month")[user_column].nunique().reset_index()
+    mau.columns = ["month", "MAU"]
+    return mau
     
