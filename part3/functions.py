9a2dfcaa950c660ecc924c51a7d1ad2677f4215a
JackNulty <jdnulty19@gmail.com>
2025-04-01 15:42:13 +0100

    #3.6 Session analysis and visualisation

--- part3/functions.py
+++ part3/functions.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
+
+# 3.2
 def count_unique_players(file_path, user_column="pid"):
     df = pd.read_csv(file_path)
     return df[user_column].nunique()
 
+# 3.3
 def calcualte_dau(file_path, date_column="Time", user_column="pid"):
     df = pd.read_csv(file_path, parse_dates=[date_column])
     df["date"] = df[date_column].dt.date
@@ -11,6 +14,7 @@ def calcualte_dau(file_path, date_column="Time", user_column="pid"):
     dau.columns = ["date", "DAU"]
     return dau
 
+# 3.4
 def calculate_mau(file_path, date_column="Time", user_column="pid"):
     df = pd.read_csv(file_path, parse_dates=[date_column])
     df["month"] = df[date_column].dt.to_period("M")
@@ -19,6 +23,7 @@ def calculate_mau(file_path, date_column="Time", user_column="pid"):
     return mau
 
 
+# 3.5
 def calcluate_stickiness(file_path):
     dau = calcualte_dau(file_path)
     mau = calculate_mau(file_path)
@@ -31,4 +36,30 @@ def calcluate_stickiness(file_path):
     stickiness_data["Stickiness"] = stickiness_data["DAU"] / stickiness_data["MAU"]
 
     return stickiness_data
+
+
+# 3.6
+def calcluate_sessions(start_file, end_file):
+    login = pd.read_csv(start_file, parse_dates=["Time"])
+    exit = pd.read_csv(end_file, parse_dates=["Time"])
     
+    sessions = pd.merge_asof(
+        login.sort_values("Time"),
+        exit.sort_values("Time"),
+        on="Time",
+        by="pid",
+        suffixes=("_login", "_exit")
+    )
+    sessions["session_duration"] = (sessions["Time"] - sessions["Time"]).dt.total_seconds() / 60
+    return sessions
+
+def calculate_median_session_duration(sessions):
+    return sessions["session_duration"].median()
+
+def calculate_sessions_per_user(sessions):
+    sessions["month"] = sessions["Time"].dt.to_period("M")
+    sessions_per_user = sessions.groupby(["month", "pid"]).size().reset_index(name="sessions_count")
+    
+    avg_sessions = sessions_per_user.groupby("month")["sessions_count"].mean().reset_index()
+    avg_sessions.columns = ["month", "avg_sessions_per_user"]
+    return avg_sessions
